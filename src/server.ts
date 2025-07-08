e4cde7ccf41fde1d1fac8f40c64e473cb4cae017
Alefer Reinert <aleferreinert@gmail.com>
2025-07-08 11:48:35 -0300

    feat: create route to get rooms

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,25 @@ import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
-  type ZodTypeProvider,
+  type ZodTypeProvider
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: env.FRONTEND_URL,
+  origin: env.FRONTEND_URL
 })
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-  return { status: 'ok' }
+  return 'Health ok!'
 })
+
+app.register(getRoomsRoute)
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log(`\x1b[1mhealth:\x1b[0m \x1b[34m${env.API_URL}/health\x1b[0m`)
