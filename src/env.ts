17dcce8d88899786c080861137d0055fee23ba6e
Alefer Reinert <aleferreinert@gmail.com>
2025-07-07 22:08:44 -0300

    refactor: format env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,10 @@
-import z from 'zod';
+import z from 'zod'
 
 const eventSchema = z.object({
   API_URL: z.string().url().default('http://localhost:3333'),
   FRONTEND_URL: z.string().url().default('http://localhost:5173'),
   NODE_ENV: z.string().default('development'),
   PORT: z.coerce.number().default(3333),
-});
+})
 
-export const env = eventSchema.parse(process.env);
+export const env = eventSchema.parse(process.env)
