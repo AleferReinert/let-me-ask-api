3834727f0489b63acdb1c52f1ecffb760ad70279
Alefer Reinert <aleferreinert@gmail.com>
2025-07-07 21:59:36 -0300

    feat: install and config biome, ultracite and husky

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,26 @@
-import fastifyCors from '@fastify/cors'
+import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 import { env } from './env.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-	origin: env.FRONTEND_URL
+  origin: env.FRONTEND_URL,
 })
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-	return { status: 'ok' }
+  return { status: 'ok' }
 })
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
-	console.log(`\x1b[1mhealth:\x1b[0m \x1b[34m${env.API_URL}/health\x1b[0m`)
-	console.log('\x1b[32m✓ HTTP Server running!\x1b[0m')
+  console.log(`\x1b[1mhealth:\x1b[0m \x1b[34m${env.API_URL}/health\x1b[0m`)
+  console.log('\x1b[32m✓ HTTP Server running!\x1b[0m')
 })
