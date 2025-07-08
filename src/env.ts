1c5a10c89346be0311f3384cb7553d584bf5236b
Alefer Reinert <aleferreinert@gmail.com>
2025-07-08 16:20:17 -0300

    fix: install dotenv and update commands

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,7 @@ const eventSchema = z.object({
   FRONTEND_URL: z.string().url().default('http://localhost:5173'),
   NODE_ENV: z.string().default('development'),
   PORT: z.coerce.number().default(3333),
-  DATABASE_URL: z.string().url().startsWith('postgresql://'),
+  DATABASE_URL: z.string().url().startsWith('postgresql://')
 })
 
 export const env = eventSchema.parse(process.env)
