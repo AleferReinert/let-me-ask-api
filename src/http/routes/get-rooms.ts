873da1510c04cf5b321a7e408cc3d4ce2ea26da2
Alefer Reinert <aleferreinert@gmail.com>
2025-07-09 13:07:28 -0300

    feat(get-rooms): add questionsCount

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,3 +1,4 @@
+import { count, eq } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
@@ -15,9 +16,15 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
       const results = await db
         .select({
           id: schema.rooms.id,
-          name: schema.rooms.name
+          name: schema.rooms.name,
+          questionsCount: count(schema.questions.id)
         })
         .from(schema.rooms)
+        .leftJoin(
+          schema.questions,
+          eq(schema.questions.roomId, schema.rooms.id)
+        )
+        .groupBy(schema.rooms.id, schema.rooms.name)
         .orderBy(schema.rooms.createAt)
       return results
     }
